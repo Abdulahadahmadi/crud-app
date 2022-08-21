a1d877955640a0c184d5a9e15a5bd3750149f516
Ahad <ab.ahmadiofficail@gmail.com>
2022-08-21 15:33:27 +0430

    search component added'

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <script setup>
+  import { ref } from 'vue'
   import Form from './components/Form.vue'
   import Filter from './components/Filter.vue'
   import Table from './components/Table.vue'
+  const users = ref(JSON.parse(localStorage.getItem('users')) || [])
+  const search = (term) => {
+    users.value = term.length > 0 ? users.value.filter(user => user.name.toLowerCase().includes(term.toLowerCase()) || user.email.toLowerCase().includes(term.toLowerCase()) || user.address.toLowerCase().includes(term.toLowerCase())) : JSON.parse(localStorage.getItem('users')) || [];
+  }
 </script>
 
 <template>
 
     <Form />
-    <Filter />
-    <Table />
-
+    <Filter @userSearched="search($event)"/>
+    <Table :users="users" />
+    
 </template>
+
 
 
